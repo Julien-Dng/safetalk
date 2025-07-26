c73ce1eb841bc6f4f469b4b1286ab19e6627044a
Julien Duong <julienduong@Juliens-MacBook-Pro.local>
2025-07-26 02:05:23 +0200

    move api key logic

--- config/firebase.ts
+++ config/firebase.ts
@@ -74,15 +74,16 @@ import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
 import { getStorage } from 'firebase/storage';
 // IMPORTANT: Utilisez cette syntaxe d'importation exacte pour AsyncStorage
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from 'expo-constants';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZ3ts77dXzyTjn4KmHgC8uKl1rw9J_MOk",
-  authDomain: "safetalk-5e4e5.firebaseapp.com",
-  projectId: "safetalk-5e4e5",
-  storageBucket: "safetalk-5e4e5.firebasestorage.app",
-  messagingSenderId: "439803992286",
-  appId: "1:439803992286:android:5457b5dd6fe8a81e218a4f",
+  apiKey: Constants.expoConfig?.extra?.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig?.extra?.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig?.extra?.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig?.extra?.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig?.extra?.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig?.extra?.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -96,7 +97,7 @@ if (getApps().length === 0) {
 // Initialize Firebase Auth with AsyncStorage persistence
 // Utilisation exacte de la syntaxe recommandée par Firebase
 export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+    persistence: getReactNativePersistence(AsyncStorage)
 });
 
 // Initialize other Firebase services
